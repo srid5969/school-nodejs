aeb1930060d6617e7b5846828142dc51fec4004f
Gowri7gs <gowrishankar@innovixtech.com>
2022-12-01 15:18:00 +0530

    update with studentattendance

--- school2/src/studentsAttendance/controller/studentsAttendance.js
+++ school2/src/studentsAttendance/controller/studentsAttendance.js
@@ -3,10 +3,10 @@ const service = require("../service/students");
 
 module.exports = router;
 
-router.get("/", async (req, res) => {
+router.get("/as", async (req, res) => {
   service.getAll().then((data) => res.json(data));
 });
-router.post('/',async(req,res)=>{
+router.post('/sa',async(req,res)=>{
   const r=req.body
   service.get((r.ClassId,r.StudentId,r.Status,r.Dates,r.Createdate))
   .then(data=>res.json(data))
